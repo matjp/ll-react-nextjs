cb0a5669d5458edd46c47956221d95bfe713fdba
Matthew J. Penwill <[email protected]>
2023-11-02 08:58:27 +1300

    Get image as base64 string from mysql

--- app/page.tsx
+++ app/page.tsx
@@ -3,7 +3,7 @@ import BookGrid from "@/components/BookGrid";
 async function getData() {
   const mysql = require('mysql2/promise');
   const connection = await mysql.createConnection({host:'localhost', user: 'api', password: 'password', database: 'library'});
-  const [rows, fields] = await connection.execute('SELECT title, author, cover_image, borrowed FROM book ORDER BY title'); 
+  const [rows, fields] = await connection.execute('SELECT title, author, TO_BASE64(cover_image) as cover_image, borrowed FROM book ORDER BY title'); 
   return rows;
 }
 
--- components/BookItem.tsx
+++ components/BookItem.tsx
@@ -1,9 +1,12 @@
-import Image from 'next/image'
+'use client'
 
-export type Book = {
+import Image from 'next/image'
+import { useState } from 'react';
+
+export interface Book {
   title: string;
   author: string;
-  cover_image: ArrayBuffer;
+  cover_image: string;
   borrowed: number;
 }
 
@@ -13,13 +16,13 @@ interface BookProps {
 
 export default function BookItem( { book } : BookProps ) {
 
-  const base64String = btoa(String.fromCharCode(...new Uint8Array(book.cover_image)));
+    const [borrowed, setBorrowed] = useState(book.borrowed);
 
   return (
     <>
       <div className="p-2 flex justify-center">
         <Image className="items-center"
-          src={`data:image/avif;base64,${base64String}`}
+          src={`data:image/avif;base64,${book.cover_image}`}
           width="242"
           height="363"
           alt={book.title}
@@ -28,7 +31,7 @@ export default function BookItem( { book } : BookProps ) {
       <div className="p-2 text-center">{book.title}</div>
       <div className="p-2 text-center">{book.author}</div>
       <div className="p-2 flex justify-center">
-        <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded items-center">Borrow</button>
+        <button onClick={ () => { setBorrowed(borrowed === 0 ? 1 : 0)} } className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded items-center">Borrow</button>
       </div>
     </>
   )
