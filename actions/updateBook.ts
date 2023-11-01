89bdd6723581d59ad22d87cb90b4b2fe6d873ec3
Matthew J. Penwill <[email protected]>
2023-11-02 10:24:17 +1300

    Set borrow button enabled state after update

--- actions/updateBook.ts
+++ actions/updateBook.ts
@@ -1,8 +1,11 @@
 'use server'
 
+import { revalidatePath } from 'next/cache'
+
 export default async function updateBook(formData: FormData) {
   const mysql = require('mysql2/promise');
   const connection = await mysql.createConnection({host:'localhost', user: 'api', password: 'password', database: 'library'});
   const updateCmd = 'update book set borrowed = 1 where title =  "' + formData.get('borrow') + '"';
   await connection.execute(updateCmd); 
+  revalidatePath('/', 'page');
 }
--- components/BookItem.tsx
+++ components/BookItem.tsx
@@ -20,7 +20,8 @@ export default function BookItem( { book } : BookProps ) {
       <div className="p-2 text-center">{book.title}</div>
       <div className="p-2 text-center">{book.author}</div>
       <form action={updateBook} className="p-2 flex justify-center">
-        <button name="borrow" value={book.title} className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded items-center">Borrow</button>
+        <button name="borrow" value={book.title} disabled={book.borrowed === 1 ? true : false}
+          className="bg-blue-500 hover:bg-blue-700 disabled:bg-gray-500 text-white font-bold py-2 px-4 rounded items-center">Borrow</button>
       </form>
     </>
   )
